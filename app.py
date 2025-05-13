ab9a302754f056d2ddc1b02ccdb2434f9236558c
Daniel <daniel.pverano@gmai.com>
2025-05-13 18:53:22 -0500

    1.0.0

---
+++ app.py
@@ -0,0 +1,19 @@
+#se importan las librerias a usar en el proyecto
+import requests
+import configparser
+
+#encargada de gestionar la renderizacion de las vistas 
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+#Se inicia la logica de la app
+
+# Esta condicion siempre va indicando 
+if __name__ =="__main__":
+    app.run(debug=True)
+    
+app.route ('/')
+# aqui va el nombre de la funcion o metodo que gestiona la ruta
+def weaher_dashboard():
+    return render_template ('home.html')
